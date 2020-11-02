7d16e64ef9899a78e7105d228a93281e75e170b7
Chamila de Alwis <chamila.sujeewa@gmail.com>
2020-11-02 23:27:32 +1300

    Read feed values from config file

--- collector/go.mod
+++ collector/go.mod
@@ -2,4 +2,7 @@ module github.com/chamilad/sinhala-blog-aggregator/collector
 
 go 1.14
 
-require github.com/mmcdole/gofeed v1.1.0
+require (
+	github.com/mmcdole/gofeed v1.1.0
+	gopkg.in/yaml.v2 v2.2.2
+)
--- collector/main.go
+++ collector/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/mmcdole/gofeed"
+	"gopkg.in/yaml.v2"
 )
 
+// 1. read feeds from file
+// 2. generate content
+// 3. store last read guid in the file
+// 4. generate content for only the newer ones
+
+type Config struct {
+	Aggr struct {
+		Collector struct {
+			Feeds []struct {
+				Name     string `yaml:"name"`
+				URL      string `yaml:"url"`
+				Feed     string `yaml:"feed"`
+				LastRead string `yaml:"lastRead"`
+			} `yaml:"feeds"`
+		} `yaml:"collector"`
+	} `yaml:"aggregator"`
+}
+
 func main() {
+	c := flag.String("c", "config.yml", "configuration file")
+	flag.Parse()
+
 	fmt.Fprintf(os.Stderr, "starting feed collector...\n")
 
-	feeds := []string{"https://monkeyseemonkeykill.wordpress.com/feed", "http://feeds.twit.tv/twit.xml"}
-	for _, feedVal := range feeds {
+	yc, err := ioutil.ReadFile(*c)
+	if err != nil {
+		log.Fatalf("error while reading config file: %s", err)
+	}
+
+	var conf Config
+	err = yaml.Unmarshal(yc, &conf)
+	if err != nil {
+		log.Fatalf("error while parsing yaml config: %s", err)
+	}
+
+	if len(conf.Aggr.Collector.Feeds) < 1 {
+		log.Fatalf("no feeds are defined in the config")
+	}
+
+	for _, feedConf := range conf.Aggr.Collector.Feeds {
 		fp := gofeed.NewParser()
-		feed, _ := fp.ParseURL(feedVal)
+		feed, _ := fp.ParseURL(feedConf.Feed)
 		fmt.Printf("title: %s\n", feed.Title)
 		for i, item := range feed.Items {
 			// guid could be used as an identifier for the last read item
